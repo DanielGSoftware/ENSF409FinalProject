226bba37bc682ea026ddd5fbba9c6292434dbfc3
DANMAN\DanGe <daniel.guieb2@gmail.com>
2018-04-04 18:35:30 -0600

    kj

--- src/FrontEnd/LoginGUI.java
+++ src/FrontEnd/LoginGUI.java
@@ -52,6 +52,14 @@ public class LoginGUI extends JFrame{
 		makeWindowListener();
 	}
 	
+	public String getUser() {
+		return userT.getText();
+	}
+	
+	public String getPass() {
+		return passT.getText();
+	}
+	
 	private void createLoginBanner() {
 		JPanel bannerPanel =  new JPanel();
 		JLabel banner = new JLabel("Login To Your Home Page");
@@ -89,24 +97,24 @@ public class LoginGUI extends JFrame{
 		signInB = new JButton("Sign In");
 		signInPanel.add(signInB);
 		signInPanel.setBackground(Color.white);
-		signInB.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("STUFF SHOULD SEND");
-				System.out.println(userT.getText());
-				System.out.println(passT.getText());
-				validLogin = theModel.loginAttempt(userT.getText(), passT.getText());
-				if(validLogin) {
-					// make a student or proff?
-					System.out.println("CLOSING");
-					close();
-				}
-				else {
-					sendError("Check user info and try again please");
-				}
-			}
-		});
+//		signInB.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println("STUFF SHOULD SEND");
+//				System.out.println(userT.getText());
+//				System.out.println(passT.getText());
+//				validLogin = theModel.loginAttempt(userT.getText(), passT.getText());
+//				if(validLogin) {
+//					// make a student or proff?
+//					System.out.println("CLOSING");
+//					close();
+//				}
+//				else {
+//					sendError("Check user info and try again please");
+//				}
+//			}
+//		});
 		
 		
 		fieldPanel.add(userPanel, BorderLayout.NORTH);
@@ -116,6 +124,10 @@ public class LoginGUI extends JFrame{
 		grandPanel.add(fieldPanel, BorderLayout.CENTER);
 		
 		container.add(grandPanel, BorderLayout.CENTER);
+	}
+	
+	public void addSignInActionListener(ActionListener aListener) {
+		signInB.addActionListener(aListener);
 	}
 	
 	private void makeWindowListener()
