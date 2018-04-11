e5c868c112de92d87fa3352499ce9363d7752d9f
HuzaifaAmar <huzaifa.amar@ucalgary.ca>
2018-04-11 16:25:28 -0600

    das

--- src/FrontEnd/StudentControl.java
+++ src/FrontEnd/StudentControl.java
@@ -73,6 +73,9 @@ public class StudentControl {
 				System.out.println("Error: when tryna get student grades, "
 						+ "database connection returned null");
 			}
+			else {
+				//add it to studentview
+			}
 		}
 	}
 	
